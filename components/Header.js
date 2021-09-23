a299e8b617f26770d863dded636471073af6072e
Matías Gutiérrez <matyuh@hotmail.com>
2021-09-23 08:17:23 -0300

    added header component routing

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
-
+import { Link } from "../routes";
 const Header = () => {
     return(
         <Menu style={{marginTop: '10px'}}>
-            <Menu.Item>
-                Cryptofunding
-            </Menu.Item>
+            <Link route='/'><a className='item'>Cryptofunding</a></Link>
             <Menu.Menu position='right'>
-                <Menu.Item>
-                    Campaigns
-                </Menu.Item>
-                <Menu.Item>
-                    +
-                </Menu.Item>
+                <Link route='/'><a className='item'>Campaigns</a></Link>
+                <Link route='/campaigns/new'><a className='item'>+</a></Link>
             </Menu.Menu>
         </Menu>
     );
