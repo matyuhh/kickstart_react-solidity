e193a73035c28dcf6c604a1e1ce7c7e3d4fa6df0
Matías Gutiérrez <matyuh@hotmail.com>
2021-09-28 07:37:21 -0300

    Created RequestRow component to render in the list of requests page + onApprove function

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Layout from '../../../components/Layout';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends React.Component {
     static async getInitialProps(props) {
         const { address } = props.query;
         const campaign = Campaign(address);
         const requestCount = await campaign.methods.getRequestsCount().call();
+        const approversCount = await campaign.methods.approversCount().call();
 
         const requests = await Promise.all(
             Array(parseInt(requestCount))
@@ -18,10 +20,24 @@ class RequestIndex extends React.Component {
                 })
         )
 
-        return { address, requests, requestCount };
+        return { address, requests, requestCount, approversCount };
+    }
+
+    renderRows() {
+        return this.props.requests.map((request, index) => {
+            return <RequestRow
+                key={index}
+                id={index}
+                request={request}
+                address={this.props.address}
+                approversCount={this.props.approversCount}
+            />;
+        });
     }
 
     render() {
+        const { Header, Row, HeaderCell, Body } = Table;
+
         return (
             <Layout>
                 <h3>Request List</h3>
@@ -30,6 +46,22 @@ class RequestIndex extends React.Component {
                         <Button primary>Add Request</Button>
                     </a>
                 </Link>
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approval Count</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+                    <Body>
+                        {this.renderRows()}
+                    </Body>
+                </Table>
             </Layout>    
         )
     }
