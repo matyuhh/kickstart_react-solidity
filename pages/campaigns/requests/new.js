5937effe137a54bbec89b0d62fa466df3ce5871d
Matías Gutiérrez <matyuh@hotmail.com>
2021-09-28 06:33:09 -0300

    added request new route

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RequestNew extends React.Component{
+    render(){
+        return (
+            <div>
+                <h3>Request New</h3>
+            </div>
+        )
+    } 
+};
+
+export default RequestNew;
