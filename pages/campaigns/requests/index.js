b446022dcc3ba84692b0b9af4fe1943a0a706eb4
Matías Gutiérrez <matyuh@hotmail.com>
2021-09-28 07:12:11 -0300

    created request variable with all the Requests structs

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Layout from '../../../components/Layout';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends React.Component {
     static async getInitialProps(props) {
         const { address } = props.query;
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestsCount().call();
 
-        return { address };
+        const requests = await Promise.all(
+            Array(parseInt(requestCount))
+                .fill()
+                .map((element,index) => {
+                    return campaign.methods.requests(index).call()
+                })
+        )
+
+        return { address, requests, requestCount };
     }
 
     render() {
